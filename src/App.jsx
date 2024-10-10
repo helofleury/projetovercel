33b1d498a733fd29f54dd4cf6467acde2eeb64af
Heloisa <heloisafleury012@gmail.com>
2024-10-10 08:51:03 -0300

    Criando o nav e ajustando o app

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
+import Nav from './components/Nav/Nav'
+import {Outlet} from 'react-router-dom'
+import Footer from './components/Footer/Footer'
 
 function App() {
   
 
   return (
     <>
-     
+     <Nav/>
+      <Outlet/>
+      <Footer/>
+    
     </>
   )
 }
